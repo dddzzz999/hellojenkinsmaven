57af34eb35dac214a4db1991848f405b1fd55e21
dz <dddzzz999@yahoo.com>
2016-08-16 00:29:19 -0400

    c

--- src/main/java/demo/HelloJenkinsM.java
+++ src/main/java/demo/HelloJenkinsM.java
@@ -3,6 +3,5 @@ package demo;
 public class HelloJenkinsM {
 	public static void main(String[] args) {
 		System.out.println("hello Jenkins Mave");
-		System.out.println("hello Jenkins Mave");
 	}
 }
