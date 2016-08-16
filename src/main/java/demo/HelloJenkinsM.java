2bdccd4ed42a2ce60178981aa56295c771dd20ff
dz <dddzzz999@yahoo.com>
2016-08-16 00:40:12 -0400

    commit & test

--- src/main/java/demo/HelloJenkinsM.java
+++ src/main/java/demo/HelloJenkinsM.java
@@ -3,5 +3,6 @@ package demo;
 public class HelloJenkinsM {
 	public static void main(String[] args) {
 		System.out.println("hello Jenkins Mave");
+		System.out.println("hello Jenkins Mave");
 	}
 }
